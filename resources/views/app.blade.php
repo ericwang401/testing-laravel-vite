95dc80a2e3fd34649a6136f17d59370489ed7a4c
Eric Wang <eric.wang401@gmail.com>
2022-06-13 23:33:07 +0200

    initialize project

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    @viteReactRefresh
+    @vite
+</head>
+
+<body>
+    test
+    <div id="app"></div>
+</body>
+
+</html>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import react from '@vitejs/plugin-react'
+// import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/main.tsx']
+        }),
+        react(),
+    ],
+})
